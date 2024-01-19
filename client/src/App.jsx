5d835e991922172cdae1aa6d07eac195f5e51961
Nel <nel.renaudin@gmail.com>
2024-01-19 19:17:43 +0100

    create react app and install chakra-ui

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Heading, Text } from "@chakra-ui/react"
+
+export default function App() {
+  return (
+    <>
+      <Heading>E-commerce app</Heading>
+      <Text>So, how are you doing?</Text>
+      <Text>I now exactly how you will respond.</Text>
+    </>
+  )
+}
