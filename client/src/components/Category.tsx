762df87661ae7bc02cd2d020ca6ee63a5943b755
Nel <nel.renaudin@gmail.com>
2024-01-21 20:59:13 +0100

    add CategoryCard zoom effect

--- client/src/components/Category.tsx
+++ client/src/components/Category.tsx
@@ -43,10 +43,17 @@ export default function Category() {
 export function CategoryCard({ category }) {
   return (
     <Link to={`/category/${category.slug}`}>
-      <div className="group  p-12 aspect-square rounded-xl flex items-end relative overflow-hidden">
-        <div className="bg-black opacity-5 absolute inset-0 -z-10" />
-        <img className="block absolute inset-0 object-cover -z-20" src={API_BASE + "/" + category.image} alt="" />
-        <h1 className="font-bold text-4xl text-white group-hover:underline">{category.name}</h1>
+      <div className="group p-12 aspect-square rounded-xl flex items-end relative overflow-hidden">
+        <div className="bg-black opacity-15 absolute inset-0 -z-10" />
+        <img
+          className="block absolute inset-0 object-cover -z-20 transition-transform duration-300 group-hover:scale-105"
+          src={API_BASE + "/" + category.image}
+          alt=""
+        />
+        <div>
+          <h1 className="font-bold text-4xl text-white">{category.name}</h1>
+          <p className="text-white mt-2 group-hover:underline">SHOP NOW</p>
+        </div>
       </div>
     </Link>
   )
