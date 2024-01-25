5ad0d7b82e2ee4467a57cba5ad4c8d51e240b10c
Nel <nel.renaudin@gmail.com>
2024-01-25 11:17:23 +0100

    update Cart

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,5 +1,5 @@
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import { BiX, BiShoppingBag } from "react-icons/bi"
+import { BiX, BiShoppingBag, BiImage } from "react-icons/bi"
 import { useCart } from "../providers/CartProvider"
 import { Link } from "react-router-dom"
 
@@ -10,27 +10,28 @@ export default function Cart() {
 
   return (
     <Sheet>
-      <SheetTrigger className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center text-xl">
+      <SheetTrigger className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center text-xl relative">
         <BiShoppingBag />
         <span className="sr-only">Cart</span>
+        <CartLabel />
       </SheetTrigger>
       <SheetContent className="w-[24rem]">
         <div className="flex flex-col h-full">
-          <header className="py-8 border-b border-gray-200 flex items-baseline justify-between">
+          <header className="px-6 py-8 border-b border-gray-200 flex items-baseline justify-between">
             <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">Cart</h1>
             <SheetClose className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center text-xl">
               <BiX />
               <span className="sr-only">Close</span>
             </SheetClose>
           </header>
-          <section className="flex-1">
+          <section className="p-6 flex-1 overflow-y-auto">
             <div className="flex flex-col gap-4">
               {items.map((i) => (
                 <CartProduct key={i._id} product={i} />
               ))}
             </div>
           </section>
-          <footer>
+          <footer className="p-6">
             <div className="py-3 flex justify-between">
               <span>Subtotal:</span>
               <span>${items.reduce((total, curr) => total + curr.price, 0)}</span>
@@ -57,26 +58,54 @@ export default function Cart() {
 }
 
 function CartProduct({ product }) {
+  const { deleteFromCart } = useCart()
+
   return (
     <div className="flex gap-4">
-      <div className="aspect-square w-24 bg-gray-200">
-        {product.image ? (
-          <img src={API_BASE + "/" + product.image} alt="" className="block w-full h-full object-cover" />
-        ) : (
-          <div className="text-5xl text-gray-400 h-full grid place-content-center opacity-50">
-            <BiImage />
-          </div>
-        )}
-      </div>
-      <div className="mt-2 grid-cols-2">
-        <h2 className="font-bold group-hover:underline">{product.name}</h2>
+      <Link to={`/product/${product.slug}`}>
+        <div className="aspect-square w-24 bg-gray-200">
+          {product.image ? (
+            <img src={API_BASE + "/" + product.image} alt="" className="block w-full h-full object-cover" />
+          ) : (
+            <div className="text-5xl text-gray-400 h-full grid place-content-center opacity-50">
+              <BiImage />
+            </div>
+          )}
+        </div>
+      </Link>
+      <div className="mt-2 flex-1 ">
+        <Link to={`/product/${product.slug}`}>
+          <h2 className="font-bold group-hover:underline">{product.name}</h2>
+        </Link>
         <p className="text-gray-600">${product.price}</p>
         <div className="flex gap-3">
           <button>Size: M</button>
-          <button>Quantity: 1</button>
-          <button>Delete</button>
+          <button>Quantity: {product.quantity}</button>
         </div>
       </div>
+      <div>
+        <button
+          onClick={() => deleteFromCart(product._id)}
+          className="w-9 h-9 rounded-md hover:bg-gray-100 grid place-content-center"
+        >
+          <BiX />
+          <span className="sr-only">Remove item</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function CartLabel() {
+  const { items } = useCart()
+
+  const nbOfItems = items.reduce((total, curr) => total + curr.quantity, 0)
+
+  if (nbOfItems === 0) return null
+
+  return (
+    <div className="absolute -right-2 -top-2 w-6 h-6 rounded-full bg-black text-white text-sm font-semibold grid place-content-center">
+      <span>{nbOfItems < 10 ? nbOfItems : "9+"}</span>
     </div>
   )
 }
--- client/src/providers/CartProvider.tsx
+++ client/src/providers/CartProvider.tsx
@@ -10,7 +10,17 @@ export default function CartProvider({ children }) {
   const closeCart = () => setIsOpen(false)
 
   const addToCart = (product) => {
-    setItems([...items, product])
+    const exists = items.find((i) => i._id === product._id)
+    if (exists) {
+      exists.quantity++
+      setItems((items) => items.map((i) => (i._id === product._id ? exists : i)))
+    } else {
+      setItems([...items, { ...product, quantity: 1 }])
+    }
+  }
+
+  const deleteFromCart = (id) => {
+    setItems((items) => items.filter((i) => i._id !== id))
   }
 
   const contextValue = {
@@ -19,6 +29,7 @@ export default function CartProvider({ children }) {
     openCart,
     closeCart,
     addToCart,
+    deleteFromCart,
   }
 
   return <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
