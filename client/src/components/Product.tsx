937f417044f850fead07622af56f464fadb2b22e
Nel <nel.renaudin@gmail.com>
2024-01-27 02:02:02 +0100

    add grid to product page

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -30,47 +30,53 @@ export default function Product() {
 
   return (
     <div className="w-[64rem] mx-auto">
-      {product.image && (
-        <div className="aspect-square bg-gray-200 mt-8">
-          <img src={API_BASE + "/" + product.image} alt="" className="block w-full h-full object-cover object-center" />
+      <div className="grid grid-cols-2 gap-8">
+        {product.image && (
+          <div className="aspect-square bg-gray-200 mt-8">
+            <img
+              src={API_BASE + "/" + product.image}
+              alt=""
+              className="block w-full h-full object-cover object-center"
+            />
+          </div>
+        )}
+        <div className="py-12">
+          <div>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">{product.name}</h1>
+            <p className="mt-2 text-xl text-gray-600">${product.price}</p>
+            <p className="text-gray-600 mt-4">{product.description}</p>
+          </div>
+          <form onSubmit={handleSubmit} className="w-96">
+            {product.sizes ? (
+              <div className="flex gap-3">
+                {product.sizes.map((s) => (
+                  <div key={s} className="flex-1">
+                    <input
+                      id={s}
+                      type="radio"
+                      name="size"
+                      value={s}
+                      onChange={(e) => setSelectedSize(e.target.value)}
+                      className="peer hidden"
+                    />
+                    <label
+                      htmlFor={s}
+                      key={s}
+                      className="block px-6 py-3 border border-gray-200 outline outline-2 outline-transparent rounded text-center peer-checked:border-blue-500 peer-checked: peer-checked:outline-blue-500 cursor-pointer select-none"
+                    >
+                      {s}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className="text-gray-600">One size</p>
+            )}
+            <button className="w-full bg-gray-900 font-semibold text-white px-6 py-3 rounded hover:bg-gray-800 mt-4">
+              Add To Cart
+            </button>
+          </form>
         </div>
-      )}
-      <div className="py-12">
-        <div>
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">{product.name}</h1>
-          <p className="mt-2 text-xl text-gray-600">${product.price}</p>
-          <p className="text-gray-600 mt-4">{product.description}</p>
-        </div>
-        <form onSubmit={handleSubmit} className="w-96">
-          {product.sizes ? (
-            <div className="flex gap-3">
-              {product.sizes.map((s) => (
-                <div key={s} className="flex-1">
-                  <input
-                    id={s}
-                    type="radio"
-                    name="size"
-                    value={s}
-                    onChange={(e) => setSelectedSize(e.target.value)}
-                    className="peer hidden"
-                  />
-                  <label
-                    htmlFor={s}
-                    key={s}
-                    className="block px-6 py-3 border border-gray-200 outline outline-2 outline-transparent rounded text-center peer-checked:border-blue-500 peer-checked: peer-checked:outline-blue-500 cursor-pointer select-none"
-                  >
-                    {s}
-                  </label>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <p className="text-gray-600">One size</p>
-          )}
-          <button className="w-full bg-gray-900 font-semibold text-white px-6 py-3 rounded hover:bg-gray-800 mt-4">
-            Add To Cart
-          </button>
-        </form>
       </div>
     </div>
   )
