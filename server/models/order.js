9b444b83f729293db3d3e7a06c8b8bb407298793
Nel <nel.renaudin@gmail.com>
2024-02-06 10:45:22 +0100

    add number property on order schema

--- server/models/order.js
+++ server/models/order.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 const orderSchema = new mongoose.Schema({
+  number: { type: Number }, // order number
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   email: { type: String, required: true },
