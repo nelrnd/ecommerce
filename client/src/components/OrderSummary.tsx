5084916734c02ef9f4c09664b81387e5ad57418c
Nel <nel.renaudin@gmail.com>
2024-02-06 10:45:45 +0100

    create OrderConfirmation page and OrderSummary component

--- client/src/components/OrderSummary.tsx
+++ client/src/components/OrderSummary.tsx
@@ -2,28 +2,28 @@ import { formatPrice } from "@/utils"
 import { Separator } from "./ui/separator"
 import ProductImage from "./ProductImage"
 
-export default function OrderSummary({ title = "Summary", products, shippingMethod }) {
-  const nbOfProducts = products.reduce((acc, curr) => acc + curr.quantity, 0)
-  const itemsLengthText = nbOfProducts + " " + (nbOfProducts > 1 ? "items" : "item")
+export default function OrderSummary({ title = "Summary", items, shippingMethod }) {
+  const nbOfitems = items.reduce((acc, curr) => acc + curr.quantity, 0)
+  const itemsLengthText = nbOfitems + " " + (nbOfitems > 1 ? "items" : "item")
   const shippingPrice = shippingMethod === "standard" ? 0 : 10
-  const subtotalPrice = products.reduce((acc, curr) => acc + curr.price * curr.quantity, 0)
+  const subtotalPrice = items.reduce((acc, curr) => acc + curr.product.price * curr.quantity, 0)
   const totalPrice = shippingPrice + subtotalPrice
 
   return (
-    <aside>
-      <header className="mb-8">
+    <aside className="bg-white p-8 rounded-2xl border border-gray-200 space-y-6">
+      <header>
         <div className="flex justify-between items-baseline">
-          <h2>{title}</h2>
+          <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">{title}</h2>
           <p className="text-sm text-gray-600">{itemsLengthText}</p>
         </div>
       </header>
       <Separator />
-      <main>
-        {products.map((product) => (
-          <OrderSummaryItem key={product._id + product.size} product={product} />
+      <main className="space-y-4">
+        {items.map((item) => (
+          <OrderSummaryItem key={item.product._id + item.size} item={item} />
         ))}
       </main>
-      <footer>
+      <footer className="pt-2 space-y-4">
         <div className="flex justify-between">
           <span>Subtotal</span>
           <span>{formatPrice(subtotalPrice)}</span>
@@ -32,7 +32,8 @@ export default function OrderSummary({ title = "Summary", products, shippingMeth
           <span>Shipping</span>
           <span>{formatPrice(shippingPrice)}</span>
         </div>
-        <div className="flex justify-between">
+        <Separator />
+        <div className="font-semibold text-lg flex justify-between">
           <span>Total</span>
           <span>{formatPrice(totalPrice)}</span>
         </div>
@@ -41,20 +42,21 @@ export default function OrderSummary({ title = "Summary", products, shippingMeth
   )
 }
 
-function OrderSummaryItem({ product }) {
-  const price = product.price * product.quantity
+function OrderSummaryItem({ item }) {
+  const { product } = item
+  const price = product.price * item.quantity
 
   return (
-    <div className="flex items-start gap-3">
+    <div className="flex items-center gap-3">
       <div className="w-16">
         <ProductImage src={product.image} className="w-" />
       </div>
       <div>
-        <h3>{product.name}</h3>
-        <p>{formatPrice(price)}</p>
-        <div className="flex gap-3">
-          <span>{product.size ? `Size: ${product.size}` : "One size"}</span>
-          <span>Quantity: {product.quantity}</span>
+        <h3 className="font-semibold">{product.name}</h3>
+        <p className="text-sm">{formatPrice(price)}</p>
+        <div className="text-sm text-gray-600 flex gap-3">
+          <span>{item.size ? `Size: ${item.size}` : "One size"}</span>
+          <span>Quantity: {item.quantity}</span>
         </div>
       </div>
     </div>
--- client/src/routes/OrderConfirmation.tsx
+++ client/src/routes/OrderConfirmation.tsx
@@ -1,13 +1,68 @@
 import { BiCheckCircle } from "react-icons/bi"
 import { buttonVariants } from "@/components/ui/button"
 import { Link } from "react-router-dom"
+import OrderSummary from "@/components/OrderSummary"
+import moment from "moment"
 
-const order = {
-  id: 81924789,
+const testOrder = {
+  _id: "65c1edfca6b6da166e7242b8",
+  number: 10002,
+  first_name: "John",
+  last_name: "Smith",
   email: "johnsmith@email.com",
+  country: "France",
+  address: "41 Rue du Louvre",
+  city: "Paris",
+  zip_code: "75000",
+  phone_code: 33,
+  phone: "631 22 42 51",
+  shipping_method: "standard",
+  products: [
+    {
+      product: {
+        _id: "65bf47580476ea150103f71e",
+        name: "Lambo",
+        slug: "lambo",
+        price: 40290,
+        image: null,
+        sizes: null,
+        created_at: "2024-02-04T08:14:16.429Z",
+        view_count: 23,
+        __v: 0,
+      },
+      quantity: 1,
+      _id: "65c1edfca6b6da166e7242b9",
+    },
+    {
+      product: {
+        _id: "65ad4a79c4ed3b0fcb48588c",
+        name: "Stussy Basic Tee",
+        slug: "stussy-basic-tee",
+        price: 40,
+        image: "images/1705866837743-stuss.webp",
+        __v: 0,
+        category: "65ad7267c8bc8a1ac7b2293b",
+        description: "This is perfect",
+        sizes: ["SMALL", "MEDIUM"],
+        view_count: 11,
+        created_at: "2024-02-06T08:30:42.046Z",
+      },
+      quantity: 2,
+      size: "SMALL",
+      _id: "65c1edfca6b6da166e7242ba",
+    },
+  ],
+  created_at: "2024-02-06T08:29:48.831Z",
+  __v: 0,
 }
 
 export default function OrderConfirmation() {
+  const creationTime = moment(testOrder.created_at)
+  const estimatedDeliveryDates =
+    testOrder.shipping_method === "standard"
+      ? creationTime.add(5, "days").format("dddd, MMM D") + " - " + creationTime.add(6, "days").format("dddd, MMM D")
+      : creationTime.add(2, "days").format("dddd, MMM D") + " - " + creationTime.add(3, "days").format("dddd, MMM D")
+
   return (
     <div className="min-h-screen bg-gray-100 px-4 py-8 lg:py-16">
       <div className="w-[40rem] max-w-full m-auto space-y-8">
@@ -20,17 +75,26 @@ export default function OrderConfirmation() {
             <p className="text-gray-600">
               We just sent you an email confirmation at:
               <br />
-              <span className="text-black font-semibold">{order.email}</span>
+              <span className="text-black font-medium">{testOrder.email}</span>
             </p>
             <p className="text-gray-600">
               Your order number is:
               <br />
-              <span className="text-black font-semibold">{order.id}</span>
+              <span className="text-black font-medium">#{testOrder.number}</span>
             </p>
             <p className="text-gray-600">
               Your order will deliver on:
               <br />
-              <span className="text-black font-semibold">Groawzy</span>
+              <span className="text-black font-medium">{estimatedDeliveryDates}</span>
+            </p>
+            <p className="text-gray-600">
+              to the address:
+              <br />
+              <span className="text-black font-medium">
+                {testOrder.address}
+                <br />
+                {testOrder.city}, {testOrder.zip_code} {testOrder.country}
+              </span>
             </p>
           </div>
           <div className="w-fit m-auto">
@@ -39,6 +103,8 @@ export default function OrderConfirmation() {
             </Link>
           </div>
         </section>
+
+        <OrderSummary title="Order summary" items={testOrder.products} shippingMethod={testOrder.shipping_method} />
       </div>
     </div>
   )
