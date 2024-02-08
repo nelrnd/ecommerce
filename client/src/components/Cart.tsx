f75e8106578c59e1cb731c5e081689b37d4cb640
Nel <nel.renaudin@gmail.com>
2024-02-08 12:27:50 +0100

    refactor Cart

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,61 +1,76 @@
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { BiX, BiShoppingBag, BiImage } from "react-icons/bi"
+import { BiX, BiShoppingBag } from "react-icons/bi"
 import { useCart } from "../providers/CartProvider"
 import { Link } from "react-router-dom"
 import { formatPrice } from "@/utils"
+import { Button } from "./ui/button"
+import ProductImage from "./ProductImage"
 
-const API_BASE = import.meta.env.VITE_API_BASE
+const MAX_ITEM_QUANTITY = 5
 
 export default function Cart() {
   const { items } = useCart()
 
-  const handleClick = () => {
-    console.log(items)
-  }
-
   return (
     <Sheet>
-      <SheetTrigger className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center text-xl relative">
-        <BiShoppingBag />
-        <span className="sr-only">Open cart</span>
-        <CartLabel />
+      <SheetTrigger asChild>
+        <button className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center relative">
+          <BiShoppingBag className="text-xl" />
+          <span className="sr-only">Open cart</span>
+          <CartBadge />
+        </button>
       </SheetTrigger>
+
       <SheetContent className="w-[24rem]">
-        <div className="flex flex-col h-full">
-          <header className="px-6 py-8 border-b border-gray-200 flex items-baseline justify-between">
-            <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">Cart</h1>
-            <SheetClose className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center text-xl">
-              <BiX />
-              <span className="sr-only">Close</span>
-            </SheetClose>
+        <div className="h-full flex flex-col">
+          <header className="p-6 border-b border-gray-200">
+            <div className="flex justify-between items-baseline">
+              <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">Cart</h1>
+              <SheetClose className="w-11 h-11 rounded-md hover:bg-gray-100 grid place-content-center">
+                <BiX className="text-xl" />
+                <span className="sr-only">Close</span>
+              </SheetClose>
+            </div>
           </header>
-          <section className="p-6 flex-1 overflow-y-auto">
-            <div className="space-y-8">
-              {items.map((i) => (
-                <CartProduct key={i._id + i.size} product={i} />
-              ))}
-            </div>
-          </section>
+          <main className="p-6 flex-1 overflow-y-auto space-y-8">
+            {items.length ? (
+              items.map((item) => <CartItem key={item.id} item={item} />)
+            ) : (
+              <div className="h-full grid place-content-center">
+                <p className="text-xl">Your cart is currently empty</p>
+              </div>
+            )}
+          </main>
           <footer className="p-6">
-            <div className="py-3 flex justify-between">
-              <span>Subtotal:</span>
-              <span>{formatPrice(items.reduce((total, curr) => total + curr.price * curr.quantity, 0))}</span>
-            </div>
-            <div className="py-3 pt-0 flex justify-between">
-              <span>Shipping:</span>
-              <span>{formatPrice(0)}</span>
-            </div>
-            <div className="font-bold py-6 border-t border-gray-200 flex justify-between">
-              <span>Total:</span>
-              <span>{formatPrice(items.reduce((total, curr) => total + curr.price * curr.quantity, 0))}</span>
-            </div>
-            <Link
-              to="/checkout"
-              className="block w-full bg-gray-900 w-96 font-semibold text-white text-center px-6 py-3 rounded hover:bg-gray-800"
-            >
+            {items.length > 0 &&
+              [
+                {
+                  text: "Subtotal",
+                  value: items.reduce((acc, curr) => curr.product.price * curr.quantity + acc, 0),
+                },
+                {
+                  text: "Shipping",
+                  value: 0,
+                },
+                {
+                  text: "Total",
+                  value: items.reduce((acc, curr) => curr.product.price * curr.quantity + acc, 0) + 0,
+                },
+              ].map((row) => (
+                <li
+                  key={row.text}
+                  className={`py-3 flex justify-between ${
+                    row.text === "Total" ? "font-bold py-6 border-t border-gray-200" : ""
+                  }`}
+                >
+                  <span>{row.text}</span>
+                  <span>{formatPrice(row.value)}</span>
+                </li>
+              ))}
+            <Button size="lg" className="w-full" disabled={items.length === 0}>
               Go To Checkout
-            </Link>
+            </Button>
           </footer>
         </div>
       </SheetContent>
@@ -63,89 +78,78 @@ export default function Cart() {
   )
 }
 
-function CartProduct({ product }) {
-  const { deleteFromCart, updateItemSize, updateItemQuantity } = useCart()
-
+function CartItem({ item, editable = true }) {
   return (
     <div className="flex gap-3">
-      <Link to={`/product/${product.slug}`}>
-        <div className="aspect-square w-20 bg-gray-200">
-          {product.image ? (
-            <img src={API_BASE + "/" + product.image} alt="" className="block w-full h-full object-cover" />
-          ) : (
-            <div className="text-5xl text-gray-400 h-full grid place-content-center opacity-50">
-              <BiImage />
-            </div>
-          )}
-        </div>
+      <Link to={`/product/${item.product.slug}`} className="w-16">
+        <ProductImage src={item.product.image} />
       </Link>
-      <div className="flex-1">
-        <div className="flex gap-2">
-          <div className="flex-1">
-            <Link to={`/product/${product.slug}`}>
-              <h2 className="font-bold hover:underline">{product.name}</h2>
-            </Link>
-            <p className="text-gray-600">{formatPrice(product.price * product.quantity)}</p>
-          </div>
-          <div>
-            <button
-              onClick={() => deleteFromCart(product)}
-              className="w-9 h-9 rounded-md hover:bg-gray-100 grid place-content-center"
-            >
-              <BiX />
-              <span className="sr-only">Remove item</span>
-            </button>
-          </div>
+      <div className="flex-1 flex gap-2">
+        <div className="flex-1">
+          <Link to={`/product/${item.product.slug}`}>
+            <h2 className="font-bold hover:underline">{item.product.name}</h2>
+          </Link>
+          <p className="text-gray-600">{formatPrice(item.product.price * item.quantity)}</p>
         </div>
-        <div className="flex gap-2 mt-1">
-          {product.sizes ? (
-            <Select defaultValue={product.size} onValueChange={(size) => updateItemSize(product, size)}>
-              <SelectTrigger className="px-2 h-8">
-                <SelectValue placeholder="Size">
-                  <span className="text-sm text-gray-600">Size:</span> {product.size}
-                </SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                {product.sizes.map((size) => (
-                  <SelectItem key={size} value={size}>
-                    {size}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          ) : (
-            <p className="text-gray-600 text-sm w-full px-2 py-1">One size</p>
-          )}
-          <Select defaultValue={product.quantity.toString()} onValueChange={(qty) => updateItemQuantity(product, qty)}>
+        <button
+          onClick={() => deleteFromCart(item)}
+          className="w-9 h-9 rounded-md hover:bg-gray-100 grid place-content-center"
+        >
+          <BiX />
+          <span className="sr-only">Remove item</span>
+        </button>
+      </div>
+      <div className="mt-1 flex gap-2">
+        {item.size && editable ? (
+          <Select value={item.size} onValueChange={(newSize) => updateItemSize(item, newSize)}>
             <SelectTrigger className="px-2 h-8">
-              <SelectValue placeholder="Quantity">
-                <span className="text-sm text-gray-600">Quantity:</span> {product.quantity}
-              </SelectValue>
+              <SelectValue placeholder="Size">Size: {item.size}</SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {item.product.sizes.map((size) => (
+                <SelectItem key={size} value={size}>
+                  {size}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        ) : (
+          <p className="text-gray-600 text-sm py-1">One size</p>
+        )}
+        {editable ? (
+          <Select
+            value={item.quantity.toString()}
+            onValueChange={(newQuantity) => updateItemQuantity(item, newQuantity)}
+          >
+            <SelectTrigger className="px-2 h-8">
+              <SelectValue placeholder="Quantity">Quantity: {item.quantity}</SelectValue>
               <SelectContent>
-                {[1, 2, 3, 4, 5].map((quantity) => (
-                  <SelectItem key={quantity} value={quantity.toString()}>
+                {Array.from(MAX_ITEM_QUANTITY, (_, index) => index + 1).map((quantity) => (
+                  <SelectItem key={quantity} value={quantity}>
                     {quantity}
                   </SelectItem>
                 ))}
               </SelectContent>
             </SelectTrigger>
           </Select>
-        </div>
+        ) : (
+          <p className="text-gray-600 text-sm py-1">Quantity: {item.quantity}</p>
+        )}
       </div>
     </div>
   )
 }
 
-function CartLabel() {
+function CartBadge() {
   const { items } = useCart()
+  const count = items.reduce((acc, curr) => acc + Number(curr.quantity), 0)
+  const text = count < 10 ? count : "9+"
 
-  const nbOfItems = items.reduce((total, curr) => total + Number(curr.quantity), 0)
-
-  if (nbOfItems === 0) return null
+  if (count === 0) return null
 
   return (
-    <div className="absolute -right-2 -top-2 w-6 h-6 rounded-full bg-black text-white text-sm font-semibold grid place-content-center">
-      <span>{nbOfItems < 10 ? nbOfItems : "9+"}</span>
+    <div className="w-5 h-5 rounded-full bg-indigo-600 text-white text-xs font-semibold grid place-content-center absolute left-2 bottom-2">
+      <span>{text}</span>
     </div>
   )
 }
