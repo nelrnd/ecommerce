f1c71a5bef5863505481c1a03746c41fe36b3841
Nel <nel.renaudin@gmail.com>
2024-02-10 19:32:01 +0100

    add select for sorting search results in Search

--- client/src/routes/Search.tsx
+++ client/src/routes/Search.tsx
@@ -3,10 +3,12 @@ import { useNavigate, useSearchParams } from "react-router-dom"
 import axios from "../axios"
 import { ProductGrid, Section } from "@/components/Layout"
 import ProductCard from "@/components/ProductCard"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 export default function Search() {
   const [searchParams] = useSearchParams()
   const term = searchParams.get("term")
+  const sort = searchParams.get("sort")
   const [results, setResults] = useState([])
   const navigate = useNavigate()
 
@@ -15,22 +17,48 @@ export default function Search() {
   useEffect(() => {
     if (term) {
       axios
-        .get(`/search?term=${term}`)
+        .get(`/search?term=${term}${sort ? "&sort=" + sort : ""}`)
         .then((res) => res.data)
         .then((results) => setResults(results))
         .catch((err) => console.log(err))
     } else {
       navigate("/")
     }
-  }, [term, navigate])
+  }, [term, navigate, sort])
+
+  function handleChange(value: string) {
+    navigate(`/search?term=${term}${value ? "&sort=" + value : ""}`)
+  }
 
   return (
     <Section className="h-full">
       {results.length > 0 ? (
         <>
           <header className="mb-8">
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">Search results for "{term}"</h1>
-            <p className="mt-2 text-xl text-gray-600">{nbOfResultsText}</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">Search results for "{term}"</h1>
+                <p className="mt-2 text-xl text-gray-600">{nbOfResultsText}</p>
+              </div>
+              {results.length > 1 && (
+                <div className="flex items-center gap-3">
+                  <span className="text-sm">Sort by</span>
+                  <Select defaultValue="latest" onValueChange={handleChange}>
+                    <SelectTrigger className="w-[11rem]">
+                      <SelectValue placeholder="Sort by" />
+                    </SelectTrigger>
+                    <SelectContent align="end">
+                      <SelectItem value="latest" defaultChecked>
+                        Latest
+                      </SelectItem>
+                      <SelectItem value="popular">Most popular</SelectItem>
+                      <SelectItem value="price_asc">Price - low to high</SelectItem>
+                      <SelectItem value="price_desc">Price - high to low</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              )}
+            </div>
           </header>
           <main>
             <ProductGrid>
