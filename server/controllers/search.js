1ba6f21d1d6f10030649f867eef0ce4fdecbdbe5
Nel <nel.renaudin@gmail.com>
2024-02-10 18:05:46 +0100

    add limit to search results in search_get controller

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,13 +1,19 @@
 const Product = require("../models/product")
 
 exports.search_get = async (req, res) => {
-  const { term } = req.query
+  const { term, limit } = req.query
 
   if (term === "") {
     return res.json([])
   }
 
-  const results = await Product.aggregate([{ $match: { name: { $regex: term, $options: "i" } } }]).limit(6)
+  const query = Product.aggregate([{ $match: { name: { $regex: term, $options: "i" } } }])
+
+  if (limit) {
+    query.limit(Number(limit))
+  }
+
+  const results = await query.exec()
 
   res.json(results)
 }
