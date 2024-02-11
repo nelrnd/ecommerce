50e2860c290801a257427d908d3d32d7d08c0a20
Nel <nel.renaudin@gmail.com>
2024-02-11 12:21:57 +0100

    hash password when creating user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,14 @@
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 exports.register = async (req, res, next) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+
   const user = new User({
     full_name: req.body.full_name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   })
 
   await user.save()
@@ -16,15 +19,15 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email }).exec()
   if (!user) {
-    return res.status(400).json({ error: "Incorrect email or password" })
+    return res.status(404).json({ error: "Incorrect email or password" })
   }
-  const match = user.password === req.body.password
+  const match = bcrypt.compareSync(req.body.password, user.password)
   if (!match) {
     return res.status(400).json({ error: "Incorrect email or password" })
   }
-  console.log(user)
   const SECRET = process.env.SECRET
   const payload = {
+    id: user._id,
     full_name: user.full_name,
     email: user.email,
     role: user.role,
