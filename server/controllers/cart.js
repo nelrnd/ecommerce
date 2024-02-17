88e4f2ace680bc5706985dd02ceb1c1d6e8af886
Nel <nel.renaudin@gmail.com>
2024-02-17 13:34:13 +0100

    update cart controller and router

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -1,52 +1,92 @@
 const Cart = require("../models/cart")
 const ProductVariant = require("../models/productVariant")
+const { body, validationResult } = require("express-validator")
 
 const MAX_ITEM_QUANTITY = 5
+
+exports.cart_get = async (req, res) => {
+  const { cartId } = req.params
+  const items = await ProductVariant.find({ in_cart: cartId }).populate("product").exec()
+  res.json(items)
+}
 
 exports.cart_create = async (req, res) => {
   const cart = new Cart({
     user: req.body.user_id,
   })
-
   await cart.save()
-
   res.json(cart)
 }
 
-exports.cart_create_item = async (req, res) => {
-  const { cartId } = req.params
-
-  const product = req.body.product
-  const sku = product.slug + "_" + req.body.size
-
-  const itemInCart = await ProductVariant.findOne({ in_cart: cartId, sku: sku }).exec()
-  if (itemInCart && itemInCart.quantity < MAX_ITEM_QUANTITY) {
-    itemInCart.quantity++
-    await itemInCart.save()
-  }
-
-  if (!itemInCart) {
-    const item = new ProductVariant({
-      sku: sku,
-      product: req.body.product._id,
-      size: req.body.size,
-      quantity: req.body.quantity,
-      in_cart: cartId,
-    })
-
-    await item.save()
-  }
-
-  const items = await ProductVariant.find({ in_cart: cartId }).populate("product").exec()
-  res.json(items)
-}
-
 exports.cart_delete = async (req, res) => {
-  const deletedCart = await Cart.findByIdAndDelete(req.body.cart_id).exec()
-
+  const { cartId } = req.params
+  const deletedCart = await Cart.findByIdAndDelete(cartId).exec()
   if (!deletedCart) {
     return res.status(404).json({ error: "Cart not found" })
   }
-
   res.json(deletedCart)
+}
+
+exports.cart_item_create = async (req, res, next) => {
+  const { cartId } = req.params
+  const { product, quantity, size } = req.body
+  const sku = product.slug + "_" + size
+
+  const itemInCart = await ProductVariant.findOne({ in_cart: cartId, sku: sku }).exec()
+
+  if (itemInCart) {
+    if (itemInCart.quantity < MAX_ITEM_QUANTITY) {
+      itemInCart.quantity++
+      await itemInCart.save()
+    }
+  } else {
+    const item = new ProductVariant({
+      sku: sku,
+      product: product._id,
+      quantity: quantity,
+      size: size,
+      in_cart: cartId,
+    })
+    await item.save()
+  }
+
+  next()
+}
+
+exports.cart_item_update = [
+  body("quantity")
+    .isNumeric()
+    .withMessage("Quantity must be a number")
+    .isInt({ max: MAX_ITEM_QUANTITY })
+    .withMessage(`Maximum quantity per item is ${MAX_ITEM_QUANTITY}`),
+  async (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.json(errors.array())
+    }
+
+    const { itemId } = req.params
+    const updatedItem = await ProductVariant.findByIdAndUpdate(
+      itemId,
+      {
+        quantity: req.body.quantity,
+        size: req.body.size,
+      },
+      { new: true }
+    ).exec()
+    if (!updatedItem) {
+      return res.status(404).json({ error: "Item not found" })
+    }
+    next()
+  },
+]
+
+exports.cart_item_delete = async (req, res, next) => {
+  const { itemId } = req.params
+  const deletedItem = await ProductVariant.findByIdAndDelete(itemId).exec()
+  if (!deletedItem) {
+    return res.status(404).json({ error: "Item not found" })
+  }
+  next()
 }
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -2,6 +2,16 @@ const express = require("express")
 const Router = express.Router()
 const cartController = require("../controllers/cart")
 
-Router.post("/:cartId", cartController.cart_create_item)
+Router.post("/", cartController.cart_create)
+
+Router.get("/:cartId", cartController.cart_get)
+
+Router.delete("/:cartId", cartController.cart_delete)
+
+Router.post("/:cartId", cartController.cart_item_create, cartController.cart_get)
+
+Router.put("/:cartId/item/:itemId", cartController.cart_item_update, cartController.cart_get)
+
+Router.delete("/:cartId/item/:itemId", cartController.cart_item_delete, cartController.cart_get)
 
 module.exports = Router
